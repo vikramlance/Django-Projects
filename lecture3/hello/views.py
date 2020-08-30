aee8ffed433c20eab985f6ac51524c46ae205959
vikramlance <vikram.lance@gmail.com>
2020-08-30 15:23:43 -0500

    aded hello app

---
+++ lecture3/hello/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    # return HttpResponse("Hello, World!")
+    return render(request, "hello/index.html")
+
+def brian(request):
+    return HttpResponse("Hello, Brian!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name):
+    # return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
